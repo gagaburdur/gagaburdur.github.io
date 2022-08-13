c9642fd67227b04a951fc7b649d09cc7eded98ed
İbrahim Berk Bulğan <berkbulgan@gmail.com>
2022-08-13 15:12:50 +0300

    Price adjustion 13.08.22

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
- mybutton = document.getElementById("goTop");
- window.onscroll = function() {scrollFunction()};
- jQuery('a.gallery').colorbox();
- 
- function scrollFunction() {
-   if (document.body.scrollTop > document.body.clientHeight || document.documentElement.scrollTop > document.documentElement.clientHeight) {
-     mybutton.style.display = "block";
-   } else {
-     mybutton.style.display = "none";
-   }
- }
+jQuery("a.gallery").colorbox();
+
+mybutton = document.getElementById("goTop");
+window.onscroll = () => {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > document.body.clientHeight ||
+    document.documentElement.scrollTop > document.documentElement.clientHeight
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
