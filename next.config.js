8eeda042e37766d536ca01f18653ae1e6834c298
İbrahim Berk Bulgan <berkbulgan@gmail.com>
2022-12-01 03:12:27 +0300

    hotfix3

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
-
 const nextConfig = {
   reactStrictMode: true,
-}
-module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
     loader: "akamai",
-    path: "",
-  },
+    path: ""
+  }
 }
 module.exports = nextConfig
