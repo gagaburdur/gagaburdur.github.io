7e72f004250ec319a57894c6713223b2b5d5997e
İbrahim Berk Bulğan <berkbulgan@gmail.com>
2026-02-20 22:13:26 +0300

    Replace allergen icon imports with AllergenIcon component in MenuItem

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,29 +1,5 @@
 import type { Allergens, MenuItem as MenuItemType } from "../menu";
-import celery from "../assets/celery.svg";
-import egg from "../assets/egg.svg";
-import fish from "../assets/fish.svg";
-import gluten from "../assets/gluten.svg";
-import milk from "../assets/milk.svg";
-import mustard from "../assets/mustard.svg";
-import nuts from "../assets/nuts.svg";
-import shell from "../assets/shell.svg";
-import soy from "../assets/soy.svg";
-import sesame from "../assets/sesame.svg";
-
-const allergenIcons: Record<string, string> = {
-  gluten,
-  susam: sesame,
-  yumurta: egg,
-  hardal: mustard,
-  sut: milk,
-  soya: soy,
-  kuruyemis: nuts,
-  yerfistigi: nuts,
-  kabuklu: shell,
-  balik: fish,
-  yemis: nuts,
-  kereviz: celery,
-};
+import AllergenIcon from "./AllergenIcon";
 
 interface MenuItemProps {
   item: MenuItemType;
@@ -50,10 +26,8 @@ const AllergenRenderer = (props: { allergens: Allergens }) => {
     <div className="flex gap-2">
       {Object.entries(props.allergens).map(([allergen, hasAllergen]) => {
         if (!hasAllergen) return null;
-        const icon = allergenIcons[allergen];
-        if (!icon) return null;
         return (
-          <img key={allergen} src={icon} alt={allergen} className="w-5 h-5 fill-[#575756]" />
+          <AllergenIcon key={allergen} name={allergen} className="w-5 h-5 text-[#575756]" />
         );
       })}
     </div>
